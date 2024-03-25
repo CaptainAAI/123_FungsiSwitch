bdfbbe0e10e42557b32c9d3437b2e8cacb9c91bf
CaptainAAI <anandaagungismail@gmail.com>
2024-03-25 10:15:37 +0700

    Daftar menu switch

--- FungsiProsedur/FungsiProsedur.cpp
+++ FungsiProsedur/FungsiProsedur.cpp
@@ -38,5 +38,12 @@ int main()
 
 	do {
 		system("cls");
+		cout << "=============== Menu ===============" << endl;
+		cout << "1. Penjumlahan" << endl;
+		cout << "2. Pengurangan" << endl;
+		cout << "3. Perkalian" << endl;
+		cout << "4. Pembagian" << endl;
+		cout << "5. Keluar" << endl;
+		cin >> pilihan;
 	}
 }
